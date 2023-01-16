834a1297f4a18e52186591bcd8776ecff2f45492
KaliMeNope <104790795+KaliMeNope@users.noreply.github.com>
2023-01-16 13:48:52 +0100

    Python

---
+++ gehaltsrechner.py
@@ -0,0 +1,10 @@
+stdlohn = input('gib Deinen Stundenlohn ein: ')
+tag = 8* int(stdlohn)
+monat = 20* tag
+jahr = 12* monat
+
+print('mein Stundenlohn ist ' + str(stdlohn) + ' €')
+print('am Tag bekomme ich ' + str(tag) +' €')
+print('im Monat werde ich bekomme ' + str(monat) +' €')
+print('im Jahr werde ich bekomme ' + str(jahr) +' €')
+input('taste drücken ...')
